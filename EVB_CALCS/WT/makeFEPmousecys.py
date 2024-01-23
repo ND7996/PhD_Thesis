97383fe75c7444f1ae6ddfcecfdd06ed3ab747e0
Nayanika Das <nayanikadas0709@gmail.com>
2024-01-23 01:27:41 +0100

    EVB/GPX6WT/

--- EVB_CALCS/WT/makeFEPmousecys.py
+++
@@ -1,7 +0,0 @@
-from Qpyl.qmakefep import make_fep
-fepstring = make_fep(qmap_file="../fepcysmouse.qmap", 
-                        pdb_file="GPX6cys_mouse.pdb", 
-                        forcefield="oplsaa",
-                        parm_files=["../../LIB/qoplsaa_all.prm"],
-                        lib_files=["../../LIB/qoplsaa.lib", "../../LIB/GPX.lib"])
-open("GPX6_wtmousecys.fep", "w").write(fepstring)
--- EVB_STRUCTURE/param.sh
+++
@@ -1,8 +0,0 @@
-pdbname=alasecala #selenol
-# first create the mae and pdb , but use the pdb file for proper numbering of all atoms to creating the ffld parameters 
-/opt/schrodinger2022-1/utilities/ffld_server -ipdb $pdbname.pdb -version 14 -print_parameters -out_file $pdbname.ffld
-q_ffld2q.py $pdbname.ffld $pdbname.pdb -o ../LIB/$pdbname
-#sed 's/cys/sec/g' ../LIB/CYS.lib > ../LIB/temp.lib   # changing the residue name in the respective files.
-#sed 's/cys/sec/g' ../LIB/CYS.prm > ../LIB/SEC.prm
-#sed 's/CYS/SEC/g' ../LIB/temp.lib > ../LIB/SEC.lib
-
