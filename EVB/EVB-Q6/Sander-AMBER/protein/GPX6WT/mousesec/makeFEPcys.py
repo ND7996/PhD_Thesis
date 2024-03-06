594dd342effca96f7ddff1230cbb5f7a8c5b5d3e
Nayanika Das <nayanikadas0709@gmail.com>
2024-03-07 00:44:03 +0100

    EVB/EVB-Q6/

--- EVB/EVB-Q6/Sander-AMBER/protein/GPX6WT/mousesec/makeFEPcys.py
+++
@@ -1,7 +0,0 @@
-from Qpyl.qmakefep import make_fep
-fepstring = make_fep(qmap_file="../fepcys.qmap", 
-                        pdb_file="GPX_wtnewcys.pdb", 
-                        forcefield="oplsaa",
-                        parm_files=["../../LIB/qoplsaa_all.prm"],
-                        lib_files=["../../LIB/qoplsaa.lib", "../../LIB/GPX.lib"])
-open("GPX6_wtnewcys.fep", "w").write(fepstring)
