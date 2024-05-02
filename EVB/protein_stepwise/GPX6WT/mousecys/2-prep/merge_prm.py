68731c9a77fb1c006a7b6a231531e3297271bb52
Nayanika Das <nayanikadas0709@gmail.com>
2024-05-02 17:46:15 +0200

    EVB/

--- EVB/protein_stepwise/GPX6WT/mousecys/2-prep/merge_prm.py
+++ EVB/protein_stepwise/GPX6WT/mousecys/2-prep/merge_prm.py
@@ -2,9 +2,19 @@ from Qpyl.core.qparameter import QPrm, QPrmError
 
 
 parameters = QPrm('oplsaa')
-parameters.read_prm('qoplsaa_all.prm')
-parameters.read_prm('HO2.prm')
+parameters.read_prm('qoplsaa.prm')
+parameters.read_prm('SNN.prm')
+parameters.read_prm('SEC.prm')
+parameters.read_prm('SEN.prm')
+parameters.read_prm('SNN.prm')
+parameters.read_prm('SUL.prm')
+parameters.read_prm('GLNP.prm')
+parameters.read_prm('SEL.prm')
+parameters.read_prm('PRX.prm')
 
-
-with open('qoplsaa.prm', 'w') as f:
+with open('qoplsaa_all.prm', 'w') as f:
     f.write(parameters.get_string())
+
+
+
+
