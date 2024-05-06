2bd2db85e253e7697a2b28932829829c63753e0d
Nayanika Das <nayanikadas0709@gmail.com>
2024-05-06 16:16:12 +0200

    EVB/

--- EVB/protein_stepwise/GPX6WT/mousecys/2-prep/makeFEPmousecys.py
+++ EVB/protein_stepwise/GPX6WT/mousecys/2-prep/makeFEPmousecys.py
@@ -1,6 +1,6 @@
 from Qpyl.qmakefep import make_fep
 fepstring = make_fep(qmap_file="fepmousecys.qmap", 
-                        pdb_file="GPX6cys_mouse.pdb", 
+                        pdb_file="GPX6cysmouse.pdb", 
                         forcefield="oplsaa",
                         parm_files=["qoplsaa_all2.prm"],
                         lib_files=["qoplsaa.lib", "GPX.lib"])
