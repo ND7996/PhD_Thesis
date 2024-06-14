142dadf322179f04ef8496df9d994b54d135c4de
Nayanika Das <nayanikadas0709@gmail.com>
2024-06-14 15:31:09 +0200

    EVB/

--- EVB/protein_concerted/GPX6WT/humancys/makeFEPhumancys.py
+++ EVB/protein_concerted/GPX6WT/humancys/makeFEPhumancys.py
@@ -1,5 +1,5 @@
 from Qpyl.qmakefep import make_fep
-fepstring = make_fep(qmap_file="fep.qmap", 
+fepstring = make_fep(qmap_file="fepcysmouse.qmap", 
                         pdb_file="GPX6cys_human.pdb", 
                         forcefield="oplsaa",
                         parm_files=["qoplsaa_all.prm"],
