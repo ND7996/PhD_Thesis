55d2275275039e447a199ef6837815e28741604f
Nayanika Das <nayanikadas0709@gmail.com>
2024-06-14 14:55:02 +0200

    EVB/

--- EVB/protein_concerted/GPX6WT/mousesec/makeFEP.py
+++ EVB/protein_concerted/GPX6WT/mousesec/makeFEP.py
@@ -1,5 +1,5 @@
 from Qpyl.qmakefep import make_fep
-fepstring = make_fep(qmap_file="fep.qmap", 
+fepstring = make_fep(qmap_file="fepsecmouse.qmap", 
                         pdb_file="GPX6sec_mouse.pdb", 
                         forcefield="oplsaa",
                         parm_files=["qoplsaa_all2.prm"],
